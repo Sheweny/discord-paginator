8326096554045e5c9b9edc702f7d5dd4970b04f8
Raphael <veauville.raphael@gmail.com>
2021-07-29 18:45:21 +0200

    Add discord buttons

--- dist/index.js
+++ dist/index.js
@@ -11,19 +11,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.EmbedPaginator = void 0;
 const array_paginator_1 = require("array-paginator");
+const discord_js_1 = require("discord.js");
 class EmbedPaginator {
     constructor(channel, pages = [], options) {
         this.channel = channel;
         this.pager = new array_paginator_1.Paginator(pages, 1);
         this.summoner = options.summoner;
         this.message = null;
-        this.emojis = {
-            first: '⏮️',
-            previous: '◀️',
-            next: '▶️',
-            last: '⏭️',
-            all: ['⏮️', '◀️', '▶️', '⏭️'],
-        };
+        // this.emojis = {
+        // 	first: '⏮️',
+        // 	previous: '◀️',
+        // 	next: '▶️',
+        // 	last: '⏭️',
+        // 	all: ['⏮️', '◀️', '▶️', '⏭️'],
+        // }
         this.init();
     }
     init() {
@@ -32,47 +33,68 @@ class EmbedPaginator {
             if (this.pager.total < 2)
                 throw new Error('A Pagination Embed must contain at least 2 pages');
             // Send the message
-            this.message = yield this.channel.send({ embeds: this.pager.first() });
+            this.message = yield this.channel.send({ embeds: this.pager.first(), components: [this.getComponents()] });
             // React to the message
-            for (const emoji in this.emojis) {
-                if (emoji === 'all')
-                    continue;
-                yield this.message.react(this.emojis[emoji]);
-            }
+            // for (const emoji in this.emojis) {
+            // 	if (emoji === 'all') continue;
+            // 	await this.message!.react(this.emojis[emoji])
+            // }
             this.listenReactions();
         });
     }
+    getComponents() {
+        const row = new discord_js_1.MessageActionRow()
+            .addComponents(new discord_js_1.MessageButton()
+            .setLabel('First')
+            .setCustomId('first')
+            .setStyle('SECONDARY')
+            .setDisabled(this.pager.hasFirst() ? false : true))
+            .addComponents(new discord_js_1.MessageButton()
+            .setLabel('Previous')
+            .setCustomId('previous')
+            .setStyle('SECONDARY')
+            .setDisabled(this.pager.hasPrevious() ? false : true))
+            .addComponents(new discord_js_1.MessageButton()
+            .setLabel('Next')
+            .setCustomId('next')
+            .setStyle('SECONDARY')
+            .setDisabled(this.pager.hasNext() ? false : true))
+            .addComponents(new discord_js_1.MessageButton()
+            .setLabel('Last')
+            .setCustomId('last')
+            .setStyle('SECONDARY')
+            .setDisabled(this.pager.hasLast() ? false : true));
+        return row;
+    }
     listenReactions() {
-        const filter = (reaction, user) => user.id === this.summoner && this.emojis.all.includes(reaction.emoji.name);
-        const collector = this.message.createReactionCollector(filter, { time: 1000 * 60 * 15 });
-        collector.on('collect', (e) => {
-            e.users.cache.map(u => {
-                if (u.id != this.message.author.id)
-                    e.users.remove(u);
-            });
-            switch (e.emoji.name) {
-                case this.emojis.first:
+        const filter = (i) => i.message.id === this.message.id && i.user.id === this.summoner;
+        const collector = this.channel.createMessageComponentCollector({ filter: filter });
+        collector.on('collect', (i) => __awaiter(this, void 0, void 0, function* () {
+            switch (i.customId) {
+                case 'first':
                     if (this.pager.first())
-                        this.changePage(this.pager.first());
+                        this.changePage(this.pager.first(), i);
                     break;
-                case this.emojis.previous:
+                case 'previous':
                     if (this.pager.hasPrevious())
-                        this.changePage(this.pager.previous());
+                        this.changePage(this.pager.previous(), i);
                     break;
-                case this.emojis.next:
+                case 'next':
                     if (this.pager.hasNext())
-                        this.changePage(this.pager.next());
+                        this.changePage(this.pager.next(), i);
                     break;
-                case this.emojis.last:
+                case 'last':
                     if (this.pager.last())
-                        this.changePage(this.pager.last());
+                        this.changePage(this.pager.last(), i);
                     break;
             }
-        });
+        }));
     }
-    changePage(page) {
-        if (page)
-            this.message.edit({ embeds: page });
+    changePage(page, interaction) {
+        interaction.update({
+            embeds: page,
+            components: [this.getComponents()]
+        });
     }
 }
 exports.EmbedPaginator = EmbedPaginator;
--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,101 @@
 import { Paginator } from 'array-paginator';
-import type { Message, MessageReaction, TextChannel, User } from 'discord.js';
+import { MessageActionRow, MessageButton } from "discord.js";
+import type { Message, ButtonInteraction, TextChannel } from 'discord.js';
 import type { IOptions, IObject } from './ts/interfaces';
 
 export class EmbedPaginator {
 	channel: TextChannel;
 	pager: Paginator<any>;
 	summoner: string;
-	emojis: IObject
+	// emojis: IObject
 	message: Message | null
 	constructor(channel: TextChannel, pages = [], options: IOptions) {
 		this.channel = channel
 		this.pager = new Paginator(pages, 1)
 		this.summoner = options.summoner
 		this.message = null;
-		this.emojis = {
-			first: '⏮️',
-			previous: '◀️',
-			next: '▶️',
-			last: '⏭️',
-			all: ['⏮️', '◀️', '▶️', '⏭️'],
-		}
+		// this.emojis = {
+		// 	first: '⏮️',
+		// 	previous: '◀️',
+		// 	next: '▶️',
+		// 	last: '⏭️',
+		// 	all: ['⏮️', '◀️', '▶️', '⏭️'],
+		// }
 		this.init()
 	}
 	async init() {
 		// Verifications :
 		if (this.pager.total < 2) throw new Error('A Pagination Embed must contain at least 2 pages')
 		// Send the message
-		this.message = await this.channel.send({ embeds: this.pager.first()! })
+
+		this.message = await this.channel.send({ embeds: this.pager.first()!, components: [this.getComponents()] })
 		// React to the message
-		for (const emoji in this.emojis) {
-			if (emoji === 'all') continue;
-			await this.message!.react(this.emojis[emoji])
-		}
+		// for (const emoji in this.emojis) {
+		// 	if (emoji === 'all') continue;
+		// 	await this.message!.react(this.emojis[emoji])
+		// }
 		this.listenReactions()
 	}
-
-	listenReactions() {
-		const filter = (reaction: MessageReaction, user: User) => user.id === this.summoner && this.emojis.all.includes(reaction.emoji.name)
-		const collector = this.message!.createReactionCollector(filter, { time: 1000 * 60 * 15 })
-		collector.on('collect', (e) => {
-			e.users.cache.map(u => {
-				if (u.id != this.message!.author.id) e.users.remove(u)
-			});
-			switch (e.emoji.name) {
-				case this.emojis.first:
-					if (this.pager.first())
-						this.changePage(this.pager.first())
-					break;
-				case this.emojis.previous:
-					if (this.pager.hasPrevious())
-						this.changePage(this.pager.previous())
-					break;
-				case this.emojis.next:
-					if (this.pager.hasNext())
-						this.changePage(this.pager.next())
-					break;
-				case this.emojis.last:
-					if (this.pager.last())
-						this.changePage(this.pager.last())
-					break;
-
-			}
-		})
+	getComponents() {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setLabel('First')
+					.setCustomId('first')
+					.setStyle('SECONDARY')
+					.setDisabled(this.pager.hasFirst() ? false : true)
+			)
+			.addComponents(
+				new MessageButton()
+					.setLabel('Previous')
+					.setCustomId('previous')
+					.setStyle('SECONDARY')
+					.setDisabled(this.pager.hasPrevious() ? false : true)
+			)
+			.addComponents(
+				new MessageButton()
+					.setLabel('Next')
+					.setCustomId('next')
+					.setStyle('SECONDARY')
+					.setDisabled(this.pager.hasNext() ? false : true)
+			)
+			.addComponents(
+				new MessageButton()
+					.setLabel('Last')
+					.setCustomId('last')
+					.setStyle('SECONDARY')
+					.setDisabled(this.pager.hasLast() ? false : true)
+			)
+		return row
 	}
-	changePage(page: any) {
-		if (page) this.message!.edit({ embeds: page })
+	listenReactions() {
+		const filter = (i: ButtonInteraction) => i.message.id === this.message!.id && i.user.id === this.summoner
+		const collector = this.channel.createMessageComponentCollector({ filter: filter })
+		collector.on('collect', async (i: ButtonInteraction) => {
+			switch (i.customId) {
+				case 'first':
+					if (this.pager.first())
+						this.changePage(this.pager.first(), i)
+					break;
+				case 'previous':
+					if (this.pager.hasPrevious())
+						this.changePage(this.pager.previous(), i)
+					break;
+				case 'next':
+					if (this.pager.hasNext())
+						this.changePage(this.pager.next(), i)
+					break;
+				case 'last':
+					if (this.pager.last())
+						this.changePage(this.pager.last(), i)
+					break;
+			}
+		});
+	}
+	changePage(page: any, interaction: ButtonInteraction) {
+		interaction.update({
+			embeds: page,
+			components: [this.getComponents()]
+		})
 	}
 };
